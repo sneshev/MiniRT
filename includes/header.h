8a22e32062ab43f7d4c259b3db92e68d4ffcd4e3
sneshev <steffal.n2@gmail.com>
2025-11-04 19:32:02 +0100

    basic structs (unfinished)

--- includes/header.h
+++ includes/header.h
@@ -6,7 +6,7 @@
 /*   By: stefuntu <stefuntu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 17:27:51 by mmisumi           #+#    #+#             */
-/*   Updated: 2025/11/04 18:37:24 by stefuntu         ###   ########.fr       */
+/*   Updated: 2025/11/04 19:08:52 by stefuntu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,6 @@
 
 # include <stdio.h>
 # include "libft.h"
-
-typedef struct s_map
-{
-	
-}	t_map;
+# include "structs.h"
 
 #endif
